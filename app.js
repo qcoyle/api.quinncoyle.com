54d6c822c41063fd5f46fc3156e8ca86ab768db8
qcoyle <quinn.t.coyle@gmail.com>
2021-10-27 04:05:41 -0500

    get to main url

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const booksRouter = require("./routes/books.js");
 app.use("/books", booksRouter);
 app.use(morgan("dev"));
 
+app.get("/", (req, res, next) => {
+    console.log("Get request to main program");
+    res.send("This is a test");
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT }`);
 });
