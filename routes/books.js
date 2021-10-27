914a5f207a6c7817eb9df9c0c970fc76b3262d6d
qcoyle <quinn.t.coyle@gmail.com>
2021-10-27 07:31:42 -0500

    added delete and util helper function

--- routes/books.js
+++ routes/books.js
@@ -4,8 +4,9 @@ const router = require("express").Router();
 const bodyParser = require("body-parser");
 const readDatabase = require("./utils").readDatabase;
 const createObjectWithID = require("./utils").createObjectWithID;
-const getMaxId = require("./utils").getMaxId;
+const getNewId = require("./utils").getNewId;
 const writeDatabase = require("./utils").writeDatabase;
+const getIndexByInnerObjectId = require("./utils").getIndexByInnerObjectId;
 
 const jsonParser = bodyParser.json();
 
@@ -25,20 +26,29 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", jsonParser, async(req, res, next) => {
-    const book = createObjectWithID(getMaxId(books) + 1, req.body);
+    const book = createObjectWithID(getNewId(books), req.body);
     books.push(book);
     await writeDatabase(books);
     res.status(201).send(book);
 });
 
 router.put("/:id", jsonParser, async(req, res, next) => {
-    const id = parseInt(req.params.id);
-    const changeIndex = books.map(x => x.id).indexOf(id); // Index of the array for update
-    const book = createObjectWithID(id, req.body);
+    const book = createObjectWithID(req.params.id, req.body);
 
-    books[changeIndex] = book; // Make the update
+    books[getIndexByInnerObjectId(req.params.id, books)] = book; // Make the update
     await writeDatabase(books);
     res.status(204).send(book);
+});
+
+router.delete("/:id", async(req, res, next) => {
+    const deleteIndex = getIndexByInnerObjectId(req.params.id, books); // Index of the array for update
+    if (deleteIndex !== -1) {
+        books.splice(deleteIndex, 1); // Remove from array at index
+        await writeDatabase(books);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
 });
 
 module.exports = router;
