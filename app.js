e882eb4a34b0ef35f2cc936c46d0b7a9cf236561
qcoyle <quinn.t.coyle@gmail.com>
2021-10-27 04:12:12 -0500

    add normalize port and shebang

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
+#!/usr/bin/env node
+
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const app = express();
-const PORT = 5501;
+
+const port = normalizePort(process.env.PORT || "5501");
+app.set("port", port);
 
 const booksRouter = require("./routes/books.js");
 
 app.use("/books", booksRouter);
-app.use(morgan("dev"));
+app.use(logger("dev"));
 
 app.get("/", (req, res, next) => {
     console.log("Get request to main program");
     res.send("This is a test");
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT }`);
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port }`);
 });
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
